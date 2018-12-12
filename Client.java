aee04e924c2d71bd4eb0041578f6b3a6682c4fb9
Choicelin <413807584@qq.com>
2018-12-12 11:39:19 +0800

    @feat: net program

---
+++ Client.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket();
+        socket.setSoTimeout(3000);
+
+        socket.connect(new InetSocketAddress(Inet4Address.getLocalHost(), 2000), 3000);
+
+        try {
+            startRun(socket);
+        } catch (Exception e) {
+        }
+
+        socket.close();
+    }
+
+    private static void startRun(Socket client) throws IOException {
+        InputStream in = System.in;
+        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+        
+        OutputStream outputStream = client.getOutputStream();
+        PrintStream socketPrintStream = new PrintStream(outputStream);
+
+        InputStream inputStream = client.getInputStream();
+        BufferedReader socketBufferedReader = new BufferedReader(
+            new InputStreamReader(inputStream));
+
+        boolean flag = true;
+        do {
+            String str = input.readLine();
+            socketPrintStream.println(str);
+
+            String echo = socketBufferedReader.readLine();
+            if ("bye".equalsIgnoreCase(echo)) {
+                flag = false;
+            } else {
+                System.out.println(echo);
+            }
+        } while (flag); 
+
+        socketPrintStream.close();
+        socketBufferedReader.close();
+    }
+}
---
+++ Server.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(2000);
+
+
+        while(true) {
+            Socket client = server.accept();
+            ClientHandler clientHandler = new ClientHandler(client);
+            clientHandler.start();
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+        private boolean flag = true;
+
+        ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            try {
+                PrintStream socketOutput = new PrintStream(socket.getOutputStream());
+                BufferedReader socketInput = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+
+                do {
+                    String str = socketInput.readLine();
+                    if ("bye".equals(str)) {
+                        socketOutput.println(str);
+                    } else {
+                        socketOutput.print(str.length());
+                    }
+                } while(flag);
+
+                socketInput.close();
+                socketOutput.close();
+            
+            } catch(Exception e) {
+
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
